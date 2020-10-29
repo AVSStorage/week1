f3237b70e3170244b8a7ee0ba9908c7f6ef9c43a
Alice <alise-fox@inbox.ru>
2020-10-29 18:28:45 +0300

    Week1 changes

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ require('http')
   
   const CORS = {
     'Access-Control-Allow-Origin': '*',
+    'X-Author' : 'alisefox',
     'Access-Control-Allow-Methods': 'GET,POST,DELETE',
     'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
 };
   res.writeHead(200, CORS);
-  if (req.url === '/v8') return res.end(process.versions.v8);
-  if (req.url === '/package.json') return require('fs').createReadStream('./package.json').pipe(res);
-  if (req.url === '/login') return res.end('alisasmirnova');
   
-  res.end('student2020heroku');
+  res.end('alisefox');
 })
 .listen(process.env.PORT);
