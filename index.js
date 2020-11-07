7608e43d1c44871366230ee3e1e8f71167564cf2
Alice <alise-fox@inbox.ru>
2020-11-07 22:13:42 +0300

    Lab4

--- index.js
+++ index.js
@@ -5,35 +5,24 @@ require('http')
 
   const CORS = {
     'Access-Control-Allow-Origin': '*',
-    'X-Author' : 'alisefox',
-    'Content-Type': 'text/plain; charset=UTF-8',
-    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
-    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers'
 };
 
   res.writeHead(200, CORS);
-  if (req.url === '/login/') {
-    return res.end('alisefox');
-  } else if(req.url === '/promise/') {
-    return res.end((function task(x) {
-      return new Promise((resolve, reject) => {
-        if (x < 18) { resolve('yes')} else { reject('no') }
-      })
-    }).toString())
-  } else if (req.url === '/fetch/') {
-    const filePath = path.join(__dirname, 'index.html');
+  if (req.url === '/result4/') {
+    const data = { message: 'alisefox', 'x-result': req.headers['x-test']}
 
-    res.writeHead(200, {
-      ...CORS,
-      'Content-Type': 'text/html'
-    });
-
-    fs.readFile(filePath, (err, html) => {
-      res.write(html);
-      res.end();
+    let body = '';
+    req.on('data', function (chunk) {
+      body += chunk;
+    }).on('end', (bodyData) => {
+      return res.end(JSON.stringify({...data, 'x-body': bodyData}))
     });
 
 
+    res.end('alisefox')
 
   } else {
 
